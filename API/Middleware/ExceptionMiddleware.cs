61c91af04d44736952642aa95bdc4d3fba5e33a5
Ahmed Mostafa <amk67482@gmail.com>
2021-07-22 10:48:48 +0200

    Extended APIs

--- API/Middleware/ExceptionMiddleware.cs
+++ API/Middleware/ExceptionMiddleware.cs
@@ -39,7 +39,7 @@ namespace API.Middleware
 
                 var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
                 var json = JsonSerializer.Serialize(response, options);
-                await context.Response.WriteAsync(json);
+                await context.Response.WriteAsync(json).ConfigureAwait(false);
             }
         }
     }
